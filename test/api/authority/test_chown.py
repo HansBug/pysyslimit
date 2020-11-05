67382306ab930808f8cc39d6f00e8ac1b3566b47
牟钰 <18373019@buaa.edu.cn>
2020-11-06 03:21:48 +0800

    user test

--- test/api/authority/test_chown.py
+++ test/api/authority/test_chown.py
@@ -9,17 +9,17 @@ from pysystem.api.authority.chown import chown
 class TestApiAuthorityChown:
 
     def setup(self):
-        print("test api authority chown start--------")
         os.mknod("./tempTest")
     
     def teardown(self):
-        print("end")
         os.remove("./tempTest")
 
 
-    def test_user_none(self):
+    def test_user_rootandnone(self):
         path = "./tempTest"
         chown(path, "root", "root")
+        assert(pwd.getpwuid(os.stat(path).st_uid).pw_name == "root")
+        chown(path)
         assert(pwd.getpwuid(os.stat(path).st_uid).pw_name == "root")
 
 
--- test/models/user/test_user.py
+++ test/models/user/test_user.py
@@ -10,7 +10,31 @@ class TestSystemUser:
     def test_1(self):
         user = SystemUser(name="root")
         assert user.name == "root"
-
+        user = SystemUser(id=0)
+        assert user.uid == 0
+        user = SystemUser()
+        assert user.uid == os.getuid()
+        assert user.passwd == pwd.getpwuid(os.getuid()).pw_passwd
+        assert user.gid == pwd.getpwuid(os.getuid()).pw_gid
+        assert user.gecos == pwd.getpwuid(os.getuid()).pw_gecos
+        assert user.dir == pwd.getpwuid(os.getuid()).pw_dir
+        assert user.shell == pwd.getpwuid(os.getuid()).pw_shell
+        assert SystemGroup(user.gid) == user.primary_group
+        _groups = user.groups
+        for _group in _groups:
+            flag = False
+            for _user in _group.users:
+                if _user.uid == user.uid:
+                    flag = True
+            assert flag
+        for _group in grp.getgrall():
+            if _group in _groups:
+                continue
+            flag = False
+            for _user in _group.users:
+                if _user.uid == user.uid:
+                    flag = True
+            assert flag == False
 
 if __name__ == "__main__":
     pytest.main([os.path.abspath(__file__)])
