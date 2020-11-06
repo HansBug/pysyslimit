44b3e0ac79d8c96a3add546ddf131b965acec591
牟钰 <18373019@buaa.edu.cn>
2020-11-07 06:55:27 +0800

    meta and del .coverage

--- setup.py
+++ setup.py
@@ -1,15 +1,24 @@
 from distutils.core import setup
+from codecs import open
 
 from setuptools import find_packages
 
 from pysystem.configs import package_version as _version
 
-_package_name = "pysystem"
+from prepare import _package_name
+
+meta = {}
+with open(os.path.join(here, _package_name, 'config', 'meta.py'), 'r', 'utf-8') as f:
+    exec(f.read(), meta)
+
 
 setup(
-    name=_package_name,
-    version=_version,
+    name=meta['__TITLE__'],
+    version=meta['__VERSION__'],
     packages=find_packages(
         include=(_package_name, "%s.*" % _package_name)
     )
+    author=meta['__AUTHOR__'],
+    author_email=meta['__AUTHOR_EMAIL__'],
+    python_requires=">=3.5"
 )
