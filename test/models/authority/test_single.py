55aba6a3110626b704bc7b9bd004e91813421dea
牟钰 <18373019@buaa.edu.cn>
2020-11-06 17:33:44 +0800

    full/base test

---
+++ test/models/authority/test_single.py
@@ -0,0 +1,13 @@
+import pytest
+
+from pysystem.models.authority import *
+
+
+@pytest.mark.unittest
+class TestModelsAuthoritySingle:
+    def test_single(self):
+        pass
+
+
+if __name__ == "__main__":
+    pytest.main([os.path.abspath(__file__)])
