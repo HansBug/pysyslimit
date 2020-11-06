dd80b6dca98fbe7400bb656c817623ab5d352274
牟钰 <18373019@buaa.edu.cn>
2020-11-06 17:47:13 +0800

    all test

--- test/models/authority/test_single.py
+++ test/models/authority/test_single.py
@@ -1,12 +1,27 @@
 import pytest
 
-from pysystem.models.authority import *
+from pysystem.models.authority.single import *
 
 
 @pytest.mark.unittest
 class TestModelsAuthoritySingle:
     def test_single(self):
-        pass
+        single = FileSingleAuthority.loads(0)
+        single.readable = 1
+        single.writable = True
+        single.executable = 0
+        assert int(single) == 6
+        assert str(single) == "rw-"
+        assert repr(single) == "<FileSingleAuthority authority: rw->"
+
+    def test_single_2(self):
+        single = FileSingleAuthority.loads("rw-")
+        assert int(single) == 6
+        assert str(single + "r-x") == "rwx"
+        assert str("r-x" + single) == "rwx"
+        assert str("r-x" | single) == "rwx"
+        assert str("r-x" - single) == "--x"
+        assert str("r-x" & single) == "r--"
 
 
 if __name__ == "__main__":
