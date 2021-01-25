7c80a6c0ad139fe39c65c0e048503e10ea9c3716
牟钰 <18373019@buaa.edu.cn>
2021-01-25 13:43:27 +0800

    fix bug in test groupadd

--- test/api/group/test_groupadd.py
+++ test/api/group/test_groupadd.py
@@ -12,20 +12,16 @@ class TestApiGroupAdd:
             groupadd(name="newgroup", force=True, gid=600, non_unique=True,
                      password="password", system=True, chroot_dir="./",
                      extra_users=True, safe=False)
-        groupdel(name="newgroup")
         assert excinfo.type == GroupaddExecuteException
 
     def test_groupadd_safe(self):
         if groupadd(name="newgroup2", safe=True):
             if groupadd(name="newgroup2", safe=True):
-                groupdel(name="newgroup2", safe=True)
                 pytest.fail("", False)
 
     def test_groupadd_normal(self):
         if not groupadd("newgroup3").name == "newgroup3":
-            groupdel(name="newgroup3", safe=True)
             pytest.fail("", False)
-        groupdel(name="newgroup3", safe=True)
 
 
 if __name__ == "__main__":
